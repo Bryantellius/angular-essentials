12aae6599e5bbc5429370ad857365a8f76edf61e
Bryantellius <brbryant2639@gmail.com>
2022-08-14 10:29:16 -0500

    UPDATE: added cart service with HttpClient, new readme

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# angular-secwji
+# Angular 'Try It' Implementations
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-secwji)
+Personal study project based from Angular's Try It tutorials focusing on architecture, component structure, navigation, data management, user input bindings, and deployment.
+
+## Tutorial References
+
+Project code started from [StackBlitz](https://stackblitz.com/edit/angular-secwji)
+
+Followed along with Angular Docs:
+
+1. [Getting Started](https://angular.io/start)
+2. [Adding Navigation](https://angular.io/start/start-routing)
+3. [Managing Data](https://angular.io/start/start-data)
+4. [Using Forms for User Input](https://angular.io/start/start-forms)
+5. [Deploying an Application](https://angular.io/start/start-deployment)
---
+++ src/app/cart.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Product } from './products';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  items: Product[] = [];
+  constructor(private http: HttpClient) {}
+
+  public addToCart(product: Product): void {
+    this.items.push(product);
+  }
+
+  public getItems(): Product[] {
+    return this.items;
+  }
+
+  public removeItem(id: number): Product[] {
+    this.items = this.items.filter((product) => product.id !== id);
+    return this.items;
+  }
+
+  public clearCart(): Product[] {
+    this.items = [];
+    return this.items;
+  }
+
+  public getShippingPrices(): Observable<{ type: string; price: number }[]> {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
+  }
+}
